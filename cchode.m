80dac0fa338c092b85e129bb81cc56ca904b8a7e
Mike-Kimani <mikalkimmani@gmail.com>
2021-06-07 10:32:57 +0300

    Case 2 - Real and repeated roots

--- cchode.m
+++ cchode.m
@@ -11,7 +11,6 @@ y_prime_0 = input('Enter the value of yprime(0): ');
 q = [a, b, c];
 w = roots(q)
     if (b*b > 4*a*c)
-        p = [a, b, c];
         fprintf('Y = C1e^%.4gx', w(1));
         fprintf(' + C2e^%.4gx',w(2));
         
@@ -23,7 +22,20 @@ w = roots(q)
         fprintf('Y = %.4ge^%.4gx', sol(1), w(1));
         fprintf(' + %.4ge^%.4gx', sol(2), w(2));
     end
-
     
+%Case 2 if the roots are real and repeated
+    if (b*b == 4*a*c)
+        
+        fprintf('Y = C1xe^%.4gx + C2e^%.4gx',w(2), w(2));
+        fprintf('Yp = C1e^%.4gx', w(1));
+        fprintf(' + %.4gC1xe^%.4gx + %.4gC2e^%.4gx\n',w(2), w(2), w(2), w(2));
+        
+        
+        C2 = y_0;
+        C1 = y_prime_0 - C2 * w(1);
+        
+
+        fprintf('Y = %.4gxe^%.4gx + %.4ge^%.4gx\n',C1 , w(2), C2, w(2));
+    end    
 
 
